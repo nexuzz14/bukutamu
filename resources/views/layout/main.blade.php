c561adacc03a99cda849e631fc616ff4669dad00
nexx14 <mnabilcf65@gmail.com>
2024-02-08 05:22:28 +0700

    tabel admin, nambah route

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="css/app.css" rel="stylesheet">
+    <link href="datatables.min.css" rel="stylesheet">
+    <script src="datatables.min.js"></script>
 </head>
 
 <body>
@@ -13,6 +15,13 @@
     @yield('container')
     @include('components.footer')
     
+
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+
+        });
+    </script>    
 </body>
 
 </html>
